940ffe94fdd54c367ba69451c52170a7d8e05b89
Haris Raees <haris.pixako1@gmail.com>
2025-09-08 03:35:52 -0700

    Auth0 working

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Auth0Provider
+      domain="dev-xtlv0bot00dc0agx.us.auth0.com"
+      clientId="PsbhEWn9k1wbvZumXRVJ2dLTtTRVVvzd"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
 )
